2578f2c8797e562b3b1a10e782e25b1c6f87512e
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-04-26 17:57:05 +0530

    errorsresolved

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,11 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: true },
-  image: { type: String, required: true },
+  image: {
+    type: String,
+    required: true,
+    default: "https://via.placeholder.com/150",
+  }, // ✅ Default image
   brand: { type: String, required: true },
   category: { type: String, required: true },
   countInStock: { type: Number, required: true },
